afb36af1f7709a29766bc77cbbbbd366762075d9
Hari Surrisyad <hari.surrisyad@gmail.com>
2024-07-02 22:26:23 +0700

    impelement search on watchlist

--- src/app/watchlist/page.tsx
+++ src/app/watchlist/page.tsx
@@ -1,16 +1,33 @@
 'use client'
 
+import { useState } from "react";
 import AccordionContent from "@/components/accordion-content";
 import { useWatchlistContext } from "@/context/WatchlistContext";
 import styles from "./page.module.css";
 
 const Watchlist = () => {
   const { watchlist } = useWatchlistContext();
+  const [keyword, setKeyword] = useState('');
+  
   return (
     <main className={styles.main}>
       <h2 className={styles.title_page}>Watchlist</h2> 
       {watchlist.length > 0 && (
-        <AccordionContent contents={watchlist}/>
+        <>
+          <input 
+            type="text"
+            placeholder="Search on watchlist"
+            className={styles.search_input}
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+          />
+          <AccordionContent 
+            contents={
+              watchlist.filter(
+                (watch) => watch.title.toLowerCase().includes(keyword.toLowerCase())
+              )}
+          />
+        </>
       )}
       {watchlist.length < 1 && (
         <span className={styles.placeholder}>Your watchlist will goes here</span>
