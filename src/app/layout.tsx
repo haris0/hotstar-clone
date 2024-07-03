980e2cf112bc0e6bf562b1acb4c350f87ca45d8d
Hari Surrisyad <hari.surrisyad@gmail.com>
2024-07-03 22:22:41 +0700

    add next top loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import WatchlistContextProvider from "@/context/WatchlistContext";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <NextTopLoader color="#ffffff"/>
       <WatchlistContextProvider>
         <Navbar />
         {children}
